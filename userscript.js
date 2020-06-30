6f4fbbcfab5d572b9254023066c7239f266a4264
Çelebi Murat <cmurat@google.com>
2020-06-30 21:02:38 +0200

    Add the main script

---
+++ userscript.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Travian Legends Bot
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://*.travian.com.tr/*
+// @grant        none
+// @require      file:///Users/cmurat/Documents/workspace/travian-tampermonkey-bot/classes.js
+// @require      file:///Users/cmurat/Documents/workspace/travian-tampermonkey-bot/travian-bot.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
