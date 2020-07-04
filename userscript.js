011eba6373802a8ee5820fdabdfdf6da33f67809
Çelebi Murat <celebi.murat@ozu.edu.tr>
2020-07-04 15:24:55 +0200

    Add dependecies

--- userscript.js
+++ userscript.js
@@ -8,6 +8,10 @@
 // @grant        none
 // @require      https://raw.githubusercontent.com/cmurat/travian-tampermonkey-bot/master/classes.js
 // @require      https://raw.githubusercontent.com/cmurat/travian-tampermonkey-bot/master/travian-bot.js
+// @require      https://raw.githubusercontent.com/cmurat/travian-tampermonkey-bot/master/upgrade.js
+// @require      https://raw.githubusercontent.com/cmurat/travian-tampermonkey-bot/master/dorf.js
+// @require      https://raw.githubusercontent.com/cmurat/travian-tampermonkey-bot/master/continuousResourceUpgrade.js
+// @require      https://raw.githubusercontent.com/cmurat/travian-tampermonkey-bot/master/messages.js
 // ==/UserScript==
 
 (function() {
